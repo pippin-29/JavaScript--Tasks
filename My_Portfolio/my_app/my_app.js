570b148f0fe2c4de95aca21c271d07e72725841e
Daniel H <operas.referee.0e@icloud.com>
2024-01-15 11:11:33 +1030

    placed frame above frame contaier

--- My_Portfolio/my_app/my_app.js
+++ My_Portfolio/my_app/my_app.js
@@ -46,3 +46,22 @@ function calculateQuotient()
 	}
 	console.log(`test`)
 }
+function calculateSubtract()
+{
+	// get the values entered by the user
+	const num1 = parseFloat(num1Input.value);
+	const num2 = parseFloat(num2Input.value);
+
+	// Check if the input is valid
+	if (isNaN(num1) || isNaN(num2))
+	{
+		resultParagraph.textContent = "Please enter valid numbers.";
+	}
+	else
+	{
+		// Calculate and display the sum
+		var sum = num1 - num2;
+		resultParagraph.textContent = "Result: " + sum
+	}
+	console.log(`test`)
+}
