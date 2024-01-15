ad9579078b0afc3dad68d053d3d95d9b6fe52645
Daniel H <operas.referee.0e@icloud.com>
2024-01-15 11:17:29 +1030

    added multiplication

--- My_Portfolio/my_app/my_app.js
+++ My_Portfolio/my_app/my_app.js
@@ -65,3 +65,22 @@ function calculateSubtract()
 	}
 	console.log(`test`)
 }
+function calculateProduct()
+{
+	// get the values entered by the user
+	const num1 = parseFloat(num1Input.value);
+	const num2 = parseFloat(num2Input.value);
+
+	// Check if the input is valid
+	if (isNaN(num1) || isNaN(num2))
+	{
+		resultParagraph.textContent = "Please enter valid numbers.";
+	}
+	else
+	{
+		// Calculate and display the sum
+		var sum = num1 * num2;
+		resultParagraph.textContent = "Result: " + sum
+	}
+	console.log(`test`)
+}
